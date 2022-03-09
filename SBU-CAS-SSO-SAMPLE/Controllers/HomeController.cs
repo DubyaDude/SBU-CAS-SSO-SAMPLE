fa3b68dc585482d81582a717d123e7f6d06c9310
Usman Shafiq <ushafiq141@gmail.com>
2022-03-09 17:16:46 -0500

    Add project files.

---
+++ SBU-CAS-SSO-SAMPLE/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SBU_CAS_SSO_SAMPLE.Models;
+using System.Diagnostics;
+
+namespace SBU_CAS_SSO_SAMPLE.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ SBU-CAS-SSO-SAMPLE/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SBU_CAS_SSO_SAMPLE.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
